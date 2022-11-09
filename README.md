258252fcc86668b41174db76e91f27563b2cab87
Jaston Ruggy <jastonruggy@icloud.com>
2022-11-09 15:44:10 +0300

    fghjkl;

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ruggy_spring_boot
+
+// Note that: --> When we write something like import javax.annotation.*; The star means all the libraries and if you just wish to take only one, Then there is were you can specify what do you want.
+// import javax.annotation.*;
---
+++ src/main/java/ruggy/com/first/model/User.java
@@ -0,0 +1,22 @@
+package ruggy.com.first.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable{
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;    
+    private String firstname;    
+    private String lastname;    
+    private String email;    
+
+    public User(){
+        // TODO Auto-generated constructor stub
+    }
+}
