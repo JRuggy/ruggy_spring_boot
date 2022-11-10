cf1ec2d48c9dfce8738e91923324e3b0a2ba5572
Jaston Ruggy <jastonruggy@icloud.com>
2022-11-10 13:46:43 +0300

    ghjk

--- src/main/java/ruggy/com/first/controller/UserController.java
+++ src/main/java/ruggy/com/first/controller/UserController.java
@@ -1,14 +1,31 @@
 package ruggy.com.first.controller;
 
+import javax.persistence.Entity;
+
+import org.apache.catalina.User;
+import ruggy.com.first.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import ruggy.com.first.user.UserRepository;
+
 @RestController
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+    
     @RequestMapping(path = {"ruggy","jarred","noway"},method = {RequestMethod.GET})
 
     public String ruggy(){
         return "This is Jaston Ruggy";
+    }
+
+    @RequestMapping(path="user",method = RequestMethod.POST)
+    public void addUser(@RequestBody User user){
+        userRepository.save(null);
     }
 }
--- src/main/java/ruggy/com/first/user/UserRepository.java
+++ src/main/java/ruggy/com/first/user/UserRepository.java
@@ -1,5 +1,12 @@
 package ruggy.com.first.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ruggy.com.first.model.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
     
 }
